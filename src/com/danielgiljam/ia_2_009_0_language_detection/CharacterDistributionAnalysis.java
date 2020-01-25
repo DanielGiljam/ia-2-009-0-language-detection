a4fb4e5b0d471030492b14a03e1868a5061a17be
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-01-25 12:08:43 +0200

    Refines print() in CharacterDistributionAnalysis

--- src/com/danielgiljam/ia_2_009_0_language_detection/CharacterDistributionAnalysis.java
+++ src/com/danielgiljam/ia_2_009_0_language_detection/CharacterDistributionAnalysis.java
@@ -9,7 +9,6 @@ abstract class CharacterDistributionAnalysis {
     private static final Pattern NON_LETTER_PATTERN = Pattern.compile("[^\\p{L} ]");
 
     private static final String SECOND_COLUMN_HEADER = "Procentuell fördelning";
-    private static final String SECOND_COLUMN_CELL_CONTENT_TEMPLATE = "%d/%d (%.2f%%)";
 
     protected final String characters;
     protected final HashMap<String, Integer> data = new HashMap<>();
@@ -25,20 +24,51 @@ abstract class CharacterDistributionAnalysis {
     abstract void print();
 
     protected void print(final String firstColumnHeader, final int firstColumnCellContentMaxWidth) {
+
+        final int firstColumnHeaderWidth = firstColumnHeader.length();
         final int firstColumnWidth = Math.max(firstColumnHeader.length(), firstColumnCellContentMaxWidth);
-        final int secondColumnWidth = Math.max(SECOND_COLUMN_HEADER.length(), getSecondColumnCellContent(total, total).length());
-        final String emptyString = "";
+        final String firstColumnCellTemplate = getFirstColumnCellTemplate(firstColumnHeaderWidth, firstColumnCellContentMaxWidth, firstColumnWidth);
+        final String secondColumnCellTemplate = getSecondColumnCellTemplate(total);
+        final int secondColumnWidth = Math.max(SECOND_COLUMN_HEADER.length(), getSecondColumnCellContent(secondColumnCellTemplate, total, total).length());
+        final String rowTemplate = getRowTemplate(firstColumnWidth, secondColumnWidth);
+
         final StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(String.format("| %-" + firstColumnWidth + "s | %-" + secondColumnWidth + "s |%n", firstColumnHeader, SECOND_COLUMN_HEADER));
-        stringBuilder.append(String.format("|%-" + (firstColumnWidth + 2) + "s|%-" + (secondColumnWidth + 2) + "s|%n", emptyString, emptyString).replace(" ", "-"));
+
+        stringBuilder.append(String.format(rowTemplate, firstColumnHeader, SECOND_COLUMN_HEADER));
+        stringBuilder.append(String.format(rowTemplate, "", "").replace(" ", "-"));
+
+        int count;
         for (final Map.Entry<String, Integer> entry : data.entrySet()) {
-            stringBuilder.append(String.format("|%-" + (firstColumnWidth + 2) + "s|%-" + (secondColumnWidth + 2) + "s|%n", entry.getKey(), entry.getValue()));
+            count = entry.getValue();
+            stringBuilder.append(String.format(
+                    rowTemplate,
+                    getFirstColumnCellContent(firstColumnCellTemplate, entry.getKey()),
+                    getSecondColumnCellContent(secondColumnCellTemplate, count, total)
+            ));
         }
+
         System.out.println(stringBuilder.toString());
     }
 
-    private static String getSecondColumnCellContent(final int count, final int total) {
-        return String.format(SECOND_COLUMN_CELL_CONTENT_TEMPLATE, count, total, (double) count / total);
+    private static String getFirstColumnCellTemplate(final int firstColumnHeaderWidth, final int firstColumnCellContentMaxWidth, final int firstColumnWidth) {
+        final int dataLossCompensation = (firstColumnHeaderWidth % 2 != 0 || firstColumnCellContentMaxWidth % 2 != 0) ? 1 : 0;
+        return firstColumnWidth != firstColumnCellContentMaxWidth ? String.format("%%%ds", firstColumnHeaderWidth / 2 + firstColumnCellContentMaxWidth / 2 + dataLossCompensation) : "%s";
+    }
+
+    private static String getFirstColumnCellContent(final String firstColumnCellTemplate, final String string) {
+        return String.format(firstColumnCellTemplate, string);
+    }
+
+    private static String getSecondColumnCellTemplate(final int total) {
+        return String.format("%%%ds/%d   ( %%5s %%%% )", Integer.toString(total).length(), total);
+    }
+
+    private static String getSecondColumnCellContent(final String secondColumnCellTemplate, final int count, final int total) {
+        return String.format(secondColumnCellTemplate, count, String.format("%.2f", (double) count / total * 100));
+    }
+
+    private static String getRowTemplate(final int firstColumnWidth, final int secondColumnWidth) {
+        return String.format("%%-" + firstColumnWidth + "s   %%-" + secondColumnWidth + "s%%n");
     }
 
     private static String removeNonLetters(final String characters) {
