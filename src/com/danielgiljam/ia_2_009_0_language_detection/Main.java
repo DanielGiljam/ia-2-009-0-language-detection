41861744c789b58479599d4a52e4593989cee100
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-01-25 12:18:19 +0200

    Implements handling of input file
    - The tool now requires an argument in order to run successfully. The argument must be a path to a text file.
    - IDEA run configurations are updated to match the new criteria for running the tool.
    - LanguageSignatures are now created for each sample data file as well as for the input file at runtime.

--- src/com/danielgiljam/ia_2_009_0_language_detection/Main.java
+++ src/com/danielgiljam/ia_2_009_0_language_detection/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,8 +14,18 @@ public class Main {
 
     private static final String PATH_TO_SAMPLE_DATA = "resources/sample-data";
 
+    private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "The tool expects the first argument to be a path to a text file.";
+
     public static void main(final String[] args) throws IOException {
         final List<TextFile> sampleDataFiles = getSampleData();
+        final List<LanguageSignature> predefinedLanguageSignatures = sampleDataFiles
+                .stream()
+                .map((final TextFile textFile) -> new LanguageSignature(textFile, textFile.getName()))
+                .collect(Collectors.toList());
+        final TextFile inputFile = getInputFile(args);
+        final LanguageSignature inputFileSignature = new LanguageSignature(inputFile);
+        inputFileSignature.print();
+        final HashMap<String, Integer> scores = inputFileSignature.scoreAgainst(predefinedLanguageSignatures);
     }
 
     private static List<TextFile> getSampleData() throws IOException {
@@ -34,6 +45,12 @@ public class Main {
                         .collect(Collectors.toList());
             }
         } else throw new DirectoryNotFoundException(sampleDataDirectory);
+    }
+
+    private static TextFile getInputFile(final String[] args) throws IOException {
+        if (args.length == 0) throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE);
+        final Path inputFilePath = Paths.get(args[0]);
+        return new TextFile(inputFilePath);
     }
 
     private static class DirectoryNotFoundException extends FileNotFoundException {
