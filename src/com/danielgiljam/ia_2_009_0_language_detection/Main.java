21f036ea6e912b09111120380972051534fba1d7
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-01-27 19:48:24 +0200

    Remakes/extends CLI + improves exception handling

--- src/com/danielgiljam/ia_2_009_0_language_detection/Main.java
+++ src/com/danielgiljam/ia_2_009_0_language_detection/Main.java
@@ -6,40 +6,57 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
-    private static final String PATH_TO_SAMPLE_DATA = "resources/sample-data";
+    private static final Pattern SAMPLE_DATA_OPTION_PATTERN = Pattern.compile("^(?:--pre-def-lang-dir|--sample-data-dir)=([\\S]+)$");
 
-    private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "The tool expects the first argument to be a path to a text file.";
+    private static final String DEFAULT_PATH_TO_SAMPLE_DATA = "resources/sample-data";
 
-    public static void main(final String[] args) throws IOException {
-        final List<TextFile> sampleDataFiles = getSampleData();
-        final List<LanguageSignature> predefinedLanguageSignatures = sampleDataFiles
+    private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_1 = "Expected at least one argument.";
+    private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_2 = "Expected argument to be a path to a text file.";
+    private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_3 = "Command contained illegal arguments.";
+
+    public static void main(final String[] args) {
+        final Args argsParsed = new Args(args);
+        if (argsParsed.printInfo) {
+            System.out.println(StringResources.INFO_MESSAGE);
+            return;
+        }
+        if (argsParsed.printUsage) {
+            System.out.println(StringResources.USAGE_MESSAGE);
+            return;
+        }
+        try {
+            final List<TextFile> sampleDataFiles = getSampleData(argsParsed.pathToSampleData);
+            final List<LanguageSignature> predefinedLanguageSignatures = sampleDataFiles
                 .stream()
                 .map((final TextFile textFile) -> new LanguageSignature(textFile, textFile.getName()))
                 .collect(Collectors.toList());
-        final TextFile inputFile = getInputFile(args);
-        final LanguageSignature inputFileSignature = new LanguageSignature(inputFile);
-        System.out.println();
-        inputFileSignature.print();
-        inputFileSignature.scoreAgainst(predefinedLanguageSignatures);
-        System.out.println(StringResources.INFO_MESSAGE);
+            final TextFile inputFile = getInputFile(argsParsed.pathToInputFile);
+            final LanguageSignature inputFileSignature = new LanguageSignature(inputFile);
+            System.out.println();
+            inputFileSignature.print();
+            inputFileSignature.scoreAgainst(predefinedLanguageSignatures);
+        } catch (final IOException|IllegalArgumentException error) {
+            crash(error);
+        }
     }
 
-    private static List<TextFile> getSampleData() throws IOException {
-        final Path sampleDataDirectory = Paths.get(PATH_TO_SAMPLE_DATA);
+    private static List<TextFile> getSampleData(final String pathToSampleData) throws IOException {
+        final Path sampleDataDirectory = Paths.get(pathToSampleData);
         if (Files.isDirectory(sampleDataDirectory)) {
             try (final Stream<Path> sampleDataFilesStream = Files.walk(sampleDataDirectory)) {
                 return sampleDataFilesStream.filter(TextFile::isTextFile)
                         .map((final Path path) -> {
                             try {
                                 return new TextFile(path);
-                            } catch (IOException error) {
-                                error.printStackTrace();
-                                System.exit(1);
+                            } catch (final IOException error) {
+                                crash(error);
                             }
                             return null;
                         })
@@ -48,10 +65,78 @@ public class Main {
         } else throw new DirectoryNotFoundException(sampleDataDirectory);
     }
 
-    private static TextFile getInputFile(final String[] args) throws IOException {
-        if (args.length == 0) throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE);
-        final Path inputFilePath = Paths.get(args[0]);
+    private static TextFile getInputFile(final String pathToInputFile) throws IOException, IllegalArgumentException {
+        if (pathToInputFile == null) throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_2);
+        final Path inputFilePath = Paths.get(pathToInputFile);
+        if (!TextFile.isTextFile(inputFilePath)) throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_2);
         return new TextFile(inputFilePath);
+    }
+
+    private static void crash(final Exception error) {
+        System.err.println(String.format(
+                StringResources.ERROR_MESSAGE_TEMPLATE,
+                error.getClass().getSimpleName(),
+                error.getMessage()
+        ));
+        System.out.println(StringResources.USAGE_MESSAGE_CRASH_VERSION);
+        System.exit(1);
+    }
+
+    private static class Args {
+
+        private boolean printInfo = false;
+        private boolean printUsage = false;
+        private String pathToSampleData = DEFAULT_PATH_TO_SAMPLE_DATA;
+        private String pathToInputFile;
+
+        private Args(final String[] args) {
+            switch (args.length) {
+                case 0:
+                    crash(new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_1));
+                    break;
+                case 1:
+                    parseSingle(args[0]);
+                    break;
+                case 2:
+                    parseTwin(args[0], args[1]);
+                    break;
+                default:
+                    crash(new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_3));
+
+            }
+        }
+
+        private void parseSingle(final String arg) {
+            switch (arg) {
+                case "-v":
+                case "--version":
+                    printInfo = true;
+                    break;
+                case "-h":
+                case "--help":
+                    printUsage = true;
+                    break;
+                default:
+                    pathToSampleData = DEFAULT_PATH_TO_SAMPLE_DATA;
+                    pathToInputFile = arg;
+            }
+        }
+
+        private void parseTwin(final String arg1, final String arg2) {
+            Matcher matcher = SAMPLE_DATA_OPTION_PATTERN.matcher(arg1);
+            if (matcher.matches()) {
+                pathToSampleData = matcher.group(1);
+                pathToInputFile = arg2;
+                return;
+            }
+            matcher = SAMPLE_DATA_OPTION_PATTERN.matcher(arg2);
+            if (matcher.matches()) {
+                pathToSampleData = matcher.group(1);
+                pathToInputFile = arg1;
+                return;
+            }
+            crash(new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_3));
+        }
     }
 
     private static class DirectoryNotFoundException extends FileNotFoundException {
--- src/com/danielgiljam/ia_2_009_0_language_detection/StringResources.java
+++ src/com/danielgiljam/ia_2_009_0_language_detection/StringResources.java
@@ -24,6 +24,46 @@ public class StringResources {
 
      static final String SCORE_TABLE_SUMMARY_TEMPLATE = "%nSpråksignaturen påminner mest om %s.%n";
 
+     static final String ERROR_MESSAGE_TEMPLATE = "%n%s: %s%n";
+
+     static final String USAGE_MESSAGE_CRASH_VERSION =
+             "Usage:\n" +
+             "\tdetectlang <path>\n" +
+             "\tdetectlang --pre-def-lang-dir=<path> <path>\n" +
+             "\tdetectlang --help\n" +
+             "\tdetectlang -v\n\n" +
+             "Options:\n" +
+             "\t-v, --version                 Shows version information about the tool.\n" +
+             "\t-h, --help                    Shows usage information about the tool.\n" +
+             "\t--pre-def-lang-dir=<path>,\n" +
+             "\t--sample-data-dir=<path>      Let's you specify where the tool looks for\n" +
+             "\t                              the text files it uses to build its\n" +
+             "\t                              predefined language signatures.\n" +
+             "\t                              If you don't specify this, the default.\n" +
+             "\t                              is \"resources/sample-data\".\n\n" +
+             "The required \"path\" -argument should point to a text file that you want the tool to \"guess\" the language of.";
+
+     static final String USAGE_MESSAGE =
+             "IA-2-009 (0) Language Detection\n\n" +
+             "Usage:\n" +
+             "\tdetectlang <path>\n" +
+             "\tdetectlang --pre-def-lang-dir=<path> <path>\n" +
+             "\tdetectlang --help\n" +
+             "\tdetectlang -v\n\n" +
+             "Options:\n" +
+             "\t-v, --version                 Shows version information about the tool.\n" +
+             "\t-h, --help                    Shows usage information about the tool.\n" +
+             "\t--pre-def-lang-dir=<path>,\n" +
+             "\t--sample-data-dir=<path>      Let's you specify where the tool looks for\n" +
+             "\t                              the text files it uses to build its\n" +
+             "\t                              predefined language signatures.\n" +
+             "\t                              If you don't specify this, the default.\n" +
+             "\t                              is \"resources/sample-data\".\n\n" +
+             "This tool analyzes the distribution characters and substrings in text files\n" +
+             "to create \"language signatures\" based on which it can \"guess\" what language\n" +
+             "an arbitrary text file in written in. The required \"path\" -argument should point\n" +
+             "to a text file that you want the tool to \"guess\" the language of.";
+
      static final String INFO_MESSAGE =
              "v1.0.0\n\n" +
              "© Daniel Giljam 2020\n" +
